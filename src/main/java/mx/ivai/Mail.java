98de4c692850daf96cfc5cc86b9341af1562a7c5
AngelSuarez03 <Angnusa11@gmail.com>
2024-10-15 14:04:41 -0600

    Corección correo presencial

--- src/main/java/mx/ivai/Mail.java
+++ src/main/java/mx/ivai/Mail.java
@@ -17,7 +17,6 @@ import mx.ivai.POJO.Registro;
 public class Mail {
     
     public static String inicializarMail(Registro registro, Cursos curso){
-        String atributoDinamico = "";
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");  
         props.put("mail.smtp.port", "587");  
@@ -42,25 +41,13 @@ public class Mail {
             if(curso.getModalidad().equals("Presencial")){
                 htmlContent = "<h2>Apreciado(a) " + registro.getNombre() + " " + registro.getApellidos() + "</h2>"
                 + "<p>El instituto Veracruzano de Acceso a la Información y Protección de Datos Personales, a través de la "
-                + "Dirección de Capacitación y Vinculación Ciudadana, le envía la dirección de la capacitación presencial denominada "
+                + "Dirección de Capacitación y Vinculación Ciudadana, le confirma su registro a la capacitación presencial denominada "
                 + "<strong>" + curso.getNombreCurso() + "</strong>, a realizarse el día <strong>" + curso.getFecha() + "</strong> a las <strong>"
                 + curso.getHora() + "</strong>.</p>"
-                + "<p><strong>" + curso.getDireccion() + "</strong></p>"
                 + "<p>Le invitamos a consultar nuestro Aviso de Privacidad en el siguiente enlace:</p>"
                 + "<p><a href='http://www.ivai.org.mx/AvisosdePrivacidad/Capacitacion/AvisosdePrivacidadCapacitacion.pdf'><strong><u>http://www.ivai.org.mx/AvisosdePrivacidad/Capacitacion/AvisosdePrivacidadCapacitacion.pdf</u></strong></a></p>"
-                + "<h3>Reglas de participación</h3>"
-                + "<ul>"
-                + "<li>Es indispensable ingresar al curso con su nombre completo.</li>"
-                + "<li>Recomendamos inscribirse con un correo que sólo ustedes utilicen para evitar errores en el registro.</li>"
-                + "<li>Los micrófonos y cámaras deben permanecer apagados durante la capacitación.</li>"
-                + "<li>Las dudas se formularán mediante el chat y se responderán al final de la capacitación.</li>"
-                + "<li>Participantes que <u>no</u> se hayan inscrito con su nombre completo en la página del instituto no podrán acreditar la evaluación.</li>"
-                + "<li>En el tema de la cuenta de Usuario en la plataforma de Moodle, les recordamos que la cuenta creada será para todos los cursos que asistan.</li>"
-                + "<li>Para poder obtener la constancia de participación, es necesario presentar la evaluación, la cual de ser aprobatoria le permitirá acceder de manera automática a ella.</li>"
-                + "</ul>"
-                + "<p><strong>Nota:</strong> Para ingresar al curso, ya sea que haya descargado o no la aplicación, solo debe oprimir el enlace electrónico.</p>"
-                + "<p>Es importante asegurarse de contar con buena conexión a internet para que no tengas fallas con la conexión.</p>"
-                + "<p>En caso de dificultad para conectarse, puede enviar un correo a: <a href='direcciondecapacitacion@ivai.org.mx'><strong><u>direcciondecapacitacion@ivai.org.mx</u></strong></a></p>";
+                + "<p>En breve recibirá la información correspondiente.</p>"
+                + "<p>En caso de tener alguna duda, puede enviar un correo a: <a href='direcciondecapacitacion@ivai.org.mx'><strong><u>direcciondecapacitacion@ivai.org.mx</u></strong></a></p>";
             }
 
             if(curso.getModalidad().equals("Virtual")){
